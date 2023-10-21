c64f55bb87a139c1efa35549dec4fbbdfef4f4e3
ferielmk <mekki.feriel2001@gmail.com>
2023-10-21 21:26:53 +0000

    final project

--- projet_vision.py
+++ projet_vision.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import random
 
 # Function to hide text in an image
 def hide_text_in_image(text, image_path, output_image_path):
@@ -46,8 +45,17 @@ def extract_text_from_image(image_path):
                 hidden_text += pixel_value[-1]
 
     text = ''
+    text_started = False  # A flag to indicate the start of text
     for i in range(0, len(hidden_text), 8):
-        text += chr(int(hidden_text[i:i + 8], 2))
+        character = hidden_text[i:i + 8]
+        if text_started:
+            if character != "00000000":  # Filter out null characters
+                text += chr(int(character, 2))
+            else:
+                break  # Stop when we encounter null characters
+        elif character != "00000000":  # Start when we encounter a non-null character
+            text_started = True
+            text += chr(int(character, 2))
 
     return text
 
@@ -55,10 +63,16 @@ def extract_text_from_image(image_path):
 text_to_hide = "This is a hidden message."
 input_image_path = "slide_1.png"
 output_image_path = "image_with_hidden_text.png"
+img_avant=cv2.imread(input_image_path,1)
+img_apres=cv2.imread(output_image_path ,1)
 
 # Hide text in the image
 hide_text_in_image(text_to_hide, input_image_path, output_image_path)
 
 # Extract hidden text from the image
 extracted_text = extract_text_from_image(output_image_path)
-print("Texte extrait de l'image:", extracted_text)
+print("Hidden Message:", extracted_text)
+cv2.imshow("image sans code secret ",img_avant)
+cv2.imshow("image avec code secret",img_apres)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
